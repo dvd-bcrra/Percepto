460724cefae9f5a568341ae5a6954548dec2e52d
David Becerra <dvd_becerra@outlook.com>
2020-05-24 00:47:47 -0700

    nada que agregar

--- app/src/main/java/com/example/percepto/DBHelper.java
+++ app/src/main/java/com/example/percepto/DBHelper.java
@@ -19,12 +19,12 @@ public class DBHelper extends SQLiteOpenHelper {
 
     //TABLA PARTICIPANTES
     public static final String PARTICIPANT_TABLE = "participants";              //  participantes
-    public static final String COLUMN_PARTICIPANT_CURP = "curp";                //  CURP - Primary key
+    public static final String COLUMN_PARTICIPANT_CURP = "curp";                //  CURP - (PK)
     public static final String COLUMN_PARTICIPANT_FIRSTNAME = "firstname";      //  nombre
     public static final String COLUMN_PARTICIPANT_LASTNAME = "lastname";        //  apellido
     public static final String COLUMN_PARTICIPANT_BIRTHDAY = "birthday";        //  fecha de nacimiento
     public static final String COLUMN_PARTICIPANT_AGE = "age";                  //  Edad
-    public static final String COLUMN_PARTICIPANT_GROUP = "group";              //  grupo
+    public static final String COLUMN_PARTICIPANT_GROUP = "grupo";              //  grupo
     public static final String COLUMN_PARTICIPANT_TIME = "time";                //  tiempo en la institucion
     public static final String COLUMN_PARTICIPANT_LEVEL = "level";              //  Nivel educativo
     public static final String COLUMN_PARTICIPANT_IQ = "iq";                    //  Coeficeinte intelectual
@@ -35,6 +35,17 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String COLUMN_PARTICIPANT_ANX = "anx";                  //  Ansiedad
     public static final String COLUMNS_PARTICIPANT_DPRS = "dprs";               //  Depresion
 
+    //TABLA EVALUACIONES 1
+    public static final String EVAL1_TABLE = "evals1";                          //  Evaluaciones 1
+    public static final String COLUMN_EVAL1_ID = "id";                          //  ID o clave de evaluacion 1 (PK)
+    public static final String COLUMN_EVAL1_CURP = "curp";                      //  Curp del participante
+    public static final String COLUMN_EVAL1_DATE = "date";                      //  Fecha de la evaluacion 1
+
+    //TABLA REGISTROS1
+    public static final String RECORDS1_TABLE = "eval1Records";                 //  Registros de la evaluacion 1
+    public static final String COLUMN_RECORDS1_EVAL1_ID = "eval1_id";           //  ID o clave de la evaluacion 1 (FK)
+    public static final String COLUMN_RECORD1_WORD = "word";                    //  Palabra a evaluar
+    public static final String COLUMN_RECORD1_SCORE = "score";                  //  calificacion dada
 
     public SQLiteDatabase db ;
 
@@ -42,20 +53,40 @@ public class DBHelper extends SQLiteOpenHelper {
         super(context, DB_NAME, null, 1);
     }
 
-
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        //CREAR TABLA USER
         db.execSQL(" CREATE TABLE " + USER_TABLE +
                  "(" + COLUMN_USER_NAME + " TEXT, "
                      + COLUMN_USER_USERNAME + " TEXT, "
                      + COLUMN_USER_PASSWORD + " TEXT, "
-                     + COLUMN_USER_CNFRMPASS + " TEXT " +")");
+                     + COLUMN_USER_CNFRMPASS + " TEXT " +");");
+
+        //CREAR TABLA PARTICIPANTE
+        db.execSQL(" CREATE TABLE " + PARTICIPANT_TABLE +
+                "(" + COLUMN_PARTICIPANT_CURP + " TEXT, "
+                + COLUMN_PARTICIPANT_FIRSTNAME + " TEXT, "
+                + COLUMN_PARTICIPANT_LASTNAME + " TEXT, "
+                + COLUMN_PARTICIPANT_BIRTHDAY + " TEXT, "
+                + COLUMN_PARTICIPANT_AGE + " INTEGER, "
+                + COLUMN_PARTICIPANT_GROUP + " TEXT, "
+                + COLUMN_PARTICIPANT_TIME + " INTEGER, "
+                + COLUMN_PARTICIPANT_LEVEL + " TEXT, "
+                + COLUMN_PARTICIPANT_IQ + " TEXT, "
+                + COLUMN_PARTICIPANT_TEPT + " TEXT, "
+                + COLUMN_PARTICIPANT_REEXP + " TEXT, "
+                + COLUMN_PARTICIPANT_AVOID + " TEXT, "
+                + COLUMN_PARTICIPANT_ACTIVE + " TEXT, "
+                + COLUMN_PARTICIPANT_ANX + " TEXT, "
+                + COLUMNS_PARTICIPANT_DPRS + " TEXT " +");");
+
     }
 
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + USER_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + USER_TABLE + ";");
         onCreate(db);
     }
 
