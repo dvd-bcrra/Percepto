db9bad1e8f778197e1266ac4d65535cce0459094
Ruben Sepulveda <ruben.sepulveda@tectijuana.edu.mx>
2020-05-25 23:09:14 -0700

    daraltas y testcompleted vistas

---
+++ app/src/main/java/com/example/percepto/DarAltas.java
@@ -0,0 +1,13 @@
+package com.example.percepto;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DarAltas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dar_altas);
+    }
+}
---
+++ app/src/main/java/com/example/percepto/Test_completed.java
@@ -0,0 +1,20 @@
+package com.example.percepto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Test_completed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_completed);
+    }
+    public void btnCompletedNext_Click(View view) {
+        Intent newIntent = new Intent(Test_completed.this,Phase1.class);
+        startActivity(newIntent);
+    }
+}
