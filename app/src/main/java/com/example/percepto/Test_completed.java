3b2cedc551d476820a7f4cc361374290d67e034b
Ruben Sepulveda <ruben.sepulveda@tectijuana.edu.mx>
2020-05-26 23:56:04 -0700

    daraltas y testcompleted vistas

--- app/src/main/java/com/example/percepto/Test_completed.java
+++ app/src/main/java/com/example/percepto/Test_completed.java
@@ -1,12 +1,25 @@
 package com.example.percepto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
 
 public class Test_completed extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +30,8 @@ public class Test_completed extends AppCompatActivity {
         Intent newIntent = new Intent(Test_completed.this,Phase1.class);
         startActivity(newIntent);
     }
+
+
     public void btnCompleted_click(View view) {
         Intent newIntent1 = new Intent(Test_completed.this,Phase1.class);
         startActivity(newIntent1);
