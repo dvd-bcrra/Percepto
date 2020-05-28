cd091cfa599d14ad48d611dad866bf53ab1ea7bd
Ruben Sepulveda <ruben.sepulveda@tectijuana.edu.mx>
2020-05-28 00:08:34 -0700

    agregado vista create account

--- app/src/main/java/com/example/percepto/Create_account.java
+++ app/src/main/java/com/example/percepto/Create_account.java
@@ -2,7 +2,9 @@ package com.example.percepto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Create_account extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class Create_account extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+    }
+    private final static String[] names = { "Ninguna", "TEPT-ASI", "CH Control Hogar",
+            "CF Contol Familia" };
+
+    public void signinBtn(View view) {
+        Intent intent = new Intent(Create_account.this, Phase1Launcher.class);
+        startActivity(intent);
     }
 }
